e6535d06ffeb149131426e10e5f671bb17601d7e
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2021-11-26 14:38:16 +0100

    Tuples with vengence...

--- Consolations/Consolations.cpp
+++ Consolations/Consolations.cpp
@@ -12,7 +12,7 @@
 
 // A specialization (the recursion stopper) of the...
 template<typename T>
-void vtprint(T const &value = T())
+void vtprint(T const &value)
 {
     std::cout << value << std::endl;
 }
@@ -44,46 +44,47 @@ int main()
         using std::views::take, std::views::transform, std::views::filter;
         using std::views::iota;
 
-        auto inv = []<typename T>(T i) { return static_cast<double>(1.0 / i); };
-        auto even = []<typename T>(T i) { return 0 == i % 0b10; };
-        auto print = []<typename T>(T i) { return std::cout << i << '\t', static_cast<T>(i); };
-
-        for (auto i : iota(-11) | filter(even) | transform(inv) | transform(print) | take(11));
+        auto lambdas = std::make_tuple([]<typename T>(T i)  { return 0 == i % 0b10; },
+                                       []<typename T>(T i)  { return static_cast<double>(01) / i; },
+                                       []<typename T>(T i)  { return std::cout << i << '\t', static_cast<T>(i); } );
+        
+        for (auto i : iota(-11)                       | 
+                      filter(std::get<0>(lambdas))    |
+                      transform(std::get<1>(lambdas)) |
+                      transform(std::get<2>(lambdas)) |
+                      take(11));
         std::cout << '\n';
+
         // https://en.cppreference.com/w/cpp/thread/future
         // https://en.cppreference.com/w/cpp/thread/promise
         // https://sodocumentation.net/cplusplus/topic/9840/futures-and-promises 
         auto fun_ptr = vtprint<int, std::string, char, double>;
-        auto a0 = std::async(std::launch::deferred, fun_ptr,
-            0100,
-            "async variadic template function call!",
-            '\100',
-            std::numbers::pi);
+        auto a_fun = std::async(std::launch::deferred, fun_ptr,
+                                0100,
+                                "async variadic template function call!",
+                                '\100',
+                                std::numbers::pi);
         auto dummy = 0;
-        std::cout << (std::cin >> dummy, a0.wait(), dummy) << std::endl;
+        std::cout << (std::cin >> dummy, a_fun.wait(), dummy) << std::endl;
     }
     {
+        // using T = std::vector<int>::const_iterator;  // One way...
         std::vector<int> v(0x1000, 0b1);
-        using T = decltype(begin(v));
+        using T = decltype(begin(v));                   // ... or another.
         std::function<int(T, T)> pms = [&pms, &v](T a, T b)
         {
             auto len = b - a;
-            if (len < 0x100)
-            {
-                return std::accumulate(a, b, 0);
-            }
-            else
-            {
-                auto m = a + len/02;
-                auto handle = std::async(std::launch::async, pms, m, b);
-                auto sum = pms(a, m);
-                return sum + handle.get();
-            }
+            if (len < 0x100) return std::accumulate(a, b, 00);
+
+            auto m = a + len/0b10;
+            auto handle = std::async(std::launch::async, pms, m, b);
+            auto sum = pms(a, m);
+            return sum + handle.get();
         };
         // A parallel recurrence (anonymous and not) example invocation...
         std::cout << std::format("Are both signed and anonymous sums made equal?\nIs {} == {}?\n",
-                                 par_sum(begin(v), end(v)),
-                                 pms(begin(v), end(v)));
+                                  par_sum(begin(v), end(v)),
+                                  pms(begin(v), end(v)));
     }
     return 0;
 }
