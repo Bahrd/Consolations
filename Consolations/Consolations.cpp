6a7b773f8c4a205f359d5ef84d9c4ab63027a7e6
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2021-11-27 10:15:04 +0100

    Ranger's Views

--- Consolations/Consolations.cpp
+++ Consolations/Consolations.cpp
@@ -1,4 +1,5 @@
 ﻿#include <numeric>
+#include <functional>
 #include <future>
 #include <iostream>
 #include <numbers>
@@ -24,7 +25,6 @@ void vtprint(T const& value, const Targs&... Fargs)
     std::cout << value << std::endl;
     vtprint(Fargs...);
 }
-
 // A parallel recursive function invocation example...
 template <typename T>
 int par_sum(T beg, T end)
@@ -38,34 +38,49 @@ int par_sum(T beg, T end)
     return sum + handle.get();
 };
 
+//auto wrt(int i, char c) { std::cout << i << c; };
 int main()
 {
+    using namespace std::placeholders;
+    //auto wrtln = std::bind(wrt, _1, 'X');
+    //wrtln(1);
+
     {
-        using std::views::take, std::views::transform, std::views::filter;
-        using std::views::iota;
+        // See e.g. https://cxx20ranges.brcha.com/#/5/19
+        using std::views::iota, 
+              std::views::filter, std::views::transform, 
+              std::views::take;
 
         auto lambdas = std::make_tuple([]<typename T>(T i)  { return 0 == i % 0b10; },
                                        []<typename T>(T i)  { return static_cast<double>(01) / i; },
-                                       []<typename T>(T i)  { return std::cout << i << '\t', static_cast<T>(i); } );
-        
-        for (auto i : iota(-11)                       | 
-                      filter(std::get<0>(lambdas))    |
-                      transform(std::get<1>(lambdas)) |
-                      transform(std::get<2>(lambdas)) |
-                      take(11));
+                                       []<typename T>(T i, char e = '\t') { return std::cout << i << e, static_cast<T>(i); });
+
+        for (auto _ : iota(-11)                    
+                    | filter(std::get<0b0>(lambdas)) | transform(std::get<0b1>(lambdas)) | transform(std::get<0b10>(lambdas)) 
+                    | take(11)
+                    );
+
         std::cout << '\n';
+        auto twice = []<typename T>(T i) -> T { return static_cast<T>(0b10) * i; };
+        auto writeline = std::bind(std::get<2>(lambdas), _1, '\n');
+
+        for (auto i : std::ranges::istream_view<double>(std::cin)
+                    | transform(twice) | transform(writeline)
+                    // Not ready for a prime time yet: http://ericniebler.github.io/range-v3/index.html
+                    // | std::ranges::ostream_iterator<double>(std::cout, "\n")
+                    );
 
         // https://en.cppreference.com/w/cpp/thread/future
         // https://en.cppreference.com/w/cpp/thread/promise
         // https://sodocumentation.net/cplusplus/topic/9840/futures-and-promises 
-        auto fun_ptr = vtprint<int, std::string, char, double>;
-        auto a_fun = std::async(std::launch::deferred, fun_ptr,
-                                0100,
-                                "async variadic template function call!",
-                                '\100',
-                                std::numbers::pi);
-        auto dummy = 0;
-        std::cout << (std::cin >> dummy, a_fun.wait(), dummy) << std::endl;
+        auto fun_fun = std::async(std::launch::deferred, 
+                                  vtprint<int, std::string, char, double>,
+                                  0100,
+                                  "async variadic template function call!",
+                                  '\100',
+                                  std::numbers::pi);
+        auto dummy = 000;
+        std::cout << (std::cin >> dummy, fun_fun.wait(), dummy) << std::endl;
     }
     {
         // using T = std::vector<int>::const_iterator;  // One way...
@@ -74,7 +89,7 @@ int main()
         std::function<int(T, T)> pms = [&pms, &v](T a, T b)
         {
             auto len = b - a;
-            if (len < 0x100) return std::accumulate(a, b, 00);
+            if (len < 0x100) return std::accumulate(a, b, 0b0);
 
             auto m = a + len/0b10;
             auto handle = std::async(std::launch::async, pms, m, b);
