e20e1c5f802ed4c5810aeeb81802e02a05b8e3ef
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2021-11-28 20:26:24 +0100

    Tuple's shuffle

--- SummAnonymous/SummAnonymous.cpp
+++ SummAnonymous/SummAnonymous.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <functional>
 #include <numeric>
+#include <execution>
 #include <future>
 #include <iostream>
 int main()
@@ -8,20 +9,33 @@ int main()
     // An anonymous and asynchronous (and almost templatized...) 
     // recursive function... 
     // A 'pms' acronym stands for 'parallel massive summation'. Yup, really... 
-    std::vector<int> v(0x1000, 0b01);
     using T = std::vector<int>::const_iterator;
+    auto v = std::vector<int>(0x100'000, 0b01);
     std::function<int(T, T)> pms = [&](T beg, T end)
     {
         auto len = end - beg;
-        if (len < 0x100) return std::accumulate(beg, end, 000);
+        if (len < 0x1000) return std::reduce(std::execution::par, beg, end, 000);
 
         auto mid = beg + len / 0b10;
         auto launch_mode = std::launch::deferred | std::launch::async;
         auto async = std::async(launch_mode, pms, mid, end);
         return pms(beg, mid) + async.get();
     };
-    // An asynchronous parallel anonymous example recursive invocation...
-    std::cout << "\nAnonymously asynchronous summa = "
-        << pms(begin(v), end(v)) << '\n';
+    // An asynchronous parallel anonymous example recursive invocation 
+    // vs the standard library one...
+    {
+        auto [tick, summa, tack] = std::tuple(std::chrono::high_resolution_clock::now(), 
+                                              pms(begin(v), end(v)),
+                                              std::chrono::high_resolution_clock::now());
+        std::cout << "Anonymously asynchronous summa = " << summa
+                  << "after " << std::chrono::duration<double, std::milli>(tick - tack) << "\n";
+    }
+    {
+        auto tick = std::chrono::high_resolution_clock::now();
+        auto summa = std::reduce(std::execution::par, begin(v), end(v), 000);
+        auto tack = std::chrono::high_resolution_clock::now();
+        std::cout << std::format("Standard asynchronous summa = {} after {}\n", summa, 
+                                 std::chrono::duration<double, std::milli>(tack - tick));
+    }
     return 0;
 }
