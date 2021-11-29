eb3964c67364d00a96ed94db46e9773ef6883ba7
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2021-11-29 09:59:11 +0100

    Bling-bling...

--- SummAnonymous/SummAnonymous.cpp
+++ SummAnonymous/SummAnonymous.cpp
@@ -1,52 +1,57 @@
-#include <vector>
+﻿#include <vector>
 #include <functional>
 #include <numeric>
 #include <execution>
 #include <future>
 #include <iostream>
+// An anonymous and asynchronous (and almost templatized...) 
+// recursive function... 
+// ... vs the standard library veterans!
 int main()
 {
-    // An anonymous and asynchronous (and almost templatized...) 
-    // recursive function... 
-    // ... vs the standard library one!
-    // A 'pms' acronym stands for 'parallel massive summation'. Yup, really... 
-    using T = std::vector<int>::const_iterator;
-    auto v = std::vector<int>(0x100'000, 0b01);
-    std::function<int(T, T)> pms = [&](T beg, T end)
+    auto v = std::vector<int>(0x10'000, 0b01);
     {
-        auto len = end - beg;
-        if (len < 0x1000) return std::reduce(std::execution::par, beg, end, 000);
-
-        auto mid = beg + len / 0b10;
-        auto launch_mode = std::launch::deferred | std::launch::async;
-        auto async = std::async(launch_mode, pms, mid, end);
-        return pms(beg, mid) + async.get();
-    };
-    {
-        auto [tick, summa, tack] = std::tuple(std::chrono::high_resolution_clock::now(), 
+        // A bling-bling flavour of the standard library routine...
+        auto [tick, summa, tack] = std::tuple(std::chrono::high_resolution_clock::now(),
                                               std::reduce(std::execution::par, begin(v), end(v)),
                                               std::chrono::high_resolution_clock::now());
         
-        std::cout << "Standard parallel summa = " << summa
+        std::cout << "Standard parallelized summa = " << summa
                   << " after " << std::fixed << std::setprecision(1)
-                  << std::chrono::duration<double, std::milli>(tick - tack) << "\n";
+                  << std::chrono::duration_cast<std::chrono::microseconds>(tick - tack) 
+                  << std::endl;
     }
     {
-        auto tick = std::chrono::high_resolution_clock::now();
+        // A 'pms' acronym stands for 'parallel massive summation'. Yup, really... 
+        using T = std::vector<int>::const_iterator;
+        std::function<int(T, T)> pms = [&](T beg, T end)
+        {
+            auto len = end - beg;
+            if (len < 0x100) return std::accumulate(beg, end, 000);
+
+            auto mid = beg + len / 0b10;
+            auto launch_mode = std::launch::deferred | std::launch::async;
+            auto async = std::async(launch_mode, pms, mid, end);
+            return pms(beg, mid) + async.get();
+        };
+
+        auto tick = std::chrono::steady_clock::now();
         auto summa = pms(begin(v), end(v));
-        auto tack = std::chrono::high_resolution_clock::now();
+        auto tack = std::chrono::steady_clock::now();
         
-        std::cout << std::format("Anonymously asynchronous summa = {} after {}\n", summa,
-                                 std::chrono::duration<double, std::milli>(tack - tick));
+        std::cout << std::format("Anonymously paralyzed summa = {} after {}\n", summa,
+                                 std::chrono::duration_cast<std::chrono::microseconds>(tack - tick));
     }
     {
-        auto [tick, summa, tack] = std::tuple(std::chrono::high_resolution_clock::now(), 
-                                              std::reduce(std::execution::seq, begin(v), end(v)),
-                                              std::chrono::high_resolution_clock::now());
+        // Just a standard library routine...
+        auto timer = std::tuple(std::chrono::system_clock::now(), 
+                                std::reduce(std::execution::seq, begin(v), end(v)),
+                                std::chrono::system_clock::now());
         
-        std::cout << "Standard sequential summa = " << summa
+        std::cout << "Standard sequential summa = " << std::get<1>(timer)
                   << " after " << std::fixed << std::setprecision(1)
-                  << std::chrono::duration<double, std::milli>(tick - tack) << "\n";
+                  << std::chrono::duration_cast<std::chrono::microseconds>(std::get<0>(timer) - std::get<2>(timer)) 
+                  << std::endl;
     }
     return 0;
 }
