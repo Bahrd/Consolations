e82494628327ca3016cf51deddee7b5dee21118e
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2022-01-06 10:13:22 +0100

    A variadic résumé....

--- VariadicAsyncT/VariadicAsyncT.cpp
+++ VariadicAsyncT/VariadicAsyncT.cpp
@@ -11,31 +11,30 @@
   */
 // A specialization (the recursion stopper) of the...
 template<typename T>
-void vtprint(T const& value)
+void varia_temp_type(T const& value)
 {
     std::cout << value << std::endl;
 }
 // ... variadic template function template
 // https://docs.microsoft.com/en-us/cpp/cpp/ellipses-and-variadic-templates?view=msvc-160
 template<typename T, typename... Targs>
-void vtprint(T const& value, const Targs&... Fargs)
+void varia_temp_type(T const& value, const Targs&... Fargs)
 {
     std::cout << value << std::endl;
-    vtprint(Fargs...);
+    varia_temp_type(Fargs...);
 }
 
 int main()
 {
     // https://en.cppreference.com/w/cpp/thread/future & https://en.cppreference.com/w/cpp/thread/promise
     // https://sodocumentation.net/cplusplus/topic/9840/futures-and-promises 
-    auto fun_fun = std::async(std::launch::deferred,
-                              vtprint<int, std::string, char, double>,
+    auto a_fun_fun = std::async(std::launch::deferred,
+                              varia_temp_type<int, std::string, char, double>,
                               0100,
-                              "async variadic template function call!",
+                              "async variadic template function!",
                               '\100',
                               std::numbers::pi);
-    auto dummy = 000;
-    std::cout << (std::cin >> dummy, fun_fun.wait(), dummy) 
+    std::cout << (varia_temp_type(010, "variadic template function!", '\10', std::numbers::e), a_fun_fun.wait(), 01)
               << std::endl;
     return 0;
 }
