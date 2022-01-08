1699b93caaba208abb48dead4d78bf0d7065678e
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2022-01-08 21:02:50 +0100

    Last'n'latch

--- CClatchCC/CClatchCC.cpp
+++ CClatchCC/CClatchCC.cpp
@@ -11,20 +11,19 @@
 using namespace std::literals;
 struct tt_profile
 {
-    std::string const name;
+    std::string const name{};
     std::string status{"Assigned"};
     std::thread action{};
 }
 dream_team[] = {{"Fabian Cancellara"}, 
                 {"Filippo Ganna"}, 
                 {"Tony Martin"}};
-
 int main() 
 {
-    std::latch team{std::size(dream_team)}, last_on_finish{0b1};
+    std::latch team{std::size(dream_team)}, 
+               last_on_finish{0b1};
     auto [t, U] = std::tuple(std::default_random_engine(std::random_device{}()), 
                              std::uniform_int_distribution<int>(0b1, 0b11));
-
     auto work = [&](tt_profile& tt) 
     {
         tt.status = tt.name + " started";      
@@ -36,16 +35,14 @@ int main()
         tt.status = tt.name + " finished";
     };
     std::cout << "TT start... begins ";
+    
     for (auto& rider : dream_team) rider.action = std::thread{work, std::ref(rider)};
-    team.wait();
-    std::cout << "TT start ends...\n";
+    team.wait(), std::cout << "& TT start ends!\n";
+    
     for (auto const& rider : dream_team) std::cout << "  " << rider.status << '\n';
-
     std::cout << "Riders approaching the finish line... "; 
-    last_on_finish.count_down();
-    for (auto& rider : dream_team) rider.action.join();
-    std::cout << " all:\n";
-    for (auto const& rider : dream_team) std::cout << "  " << rider.status << '\n';
+    last_on_finish.count_down(), std::cout << " all of'em!:\n";
 
+    for (auto& rider : dream_team) rider.action.join(), std::cout << "  " << rider.status << '\n';  
     return 0;
 }
