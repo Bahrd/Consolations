606cfc1a80563216ab3d6418e573e0eb66b70e35
Przemysław Śliwiński, Ph.D., D.Sc. <Przemyslaw.M.Sliwinski@windowslive.com>
2024-01-28 10:21:25 +0100

    baba, abba, baca & cba

--- ArithmetiC/Program.cs
+++ ArithmetiC/Program.cs
@@ -12,35 +12,33 @@
          * "PMS"
          * Taken from nowhere...
          */
-        const int size = 2;
-        const double p = 1.0/64.0;
-        readonly int[] letterFreqMap = new int[size];
-        readonly int[] letterFreqUnmap = new int[size] {
-                                                0   ,
-                                                1   /*,
-                                                2	,
-                                                3  ,
-                                                4  ,
-                                                5  ,
-                                                6  ,
-                                                7*/
-                                               };
+        const int size = 3;
+        const double p = 1 / 4.0;
+        const double pa = 1.0 - p, pb = 3*p/4, pc = p/4;
 
-        readonly double[] letterFreq = new double[size]
-                                                {
-                                                       1.0 - p,
-                                                       p
-                                                    /*,
-                                                       1.0/64.0
-                                                    0.000488281,
-                                                    0.000976563,
-                                                    0.001953125,
-                                                    0.00390625,
-                                                    0.0078125,
-                                                    0.015625,
-                                                    0.03125,
-                                                    0.937988281*/
-                                                    /*0.000976563,
+        readonly int[] letterFreqMap = new int[size];
+        readonly int[] letterFreqUnmap = new int[size] { 0, 1, 2 
+                                                        /*,
+                                                        2	,
+                                                        3  ,
+                                                        4  ,
+                                                        5  ,
+                                                        6  ,
+                                                        7*/
+                                                       };
+
+        readonly double[] letterFreq = new double[size] { pa, pb, pc
+                                                        /*,
+                                                           1.0/64.0
+                                                        0.000488281,
+                                                        0.000976563,
+                                                        0.001953125,
+                                                        0.00390625,
+                                                        0.0078125,
+                                                        0.015625,
+                                                        0.03125,
+                                                        0.937988281*/
+                                                        /*0.000976563,
                                                     0.001953125,
                                                     0.00390625,
                                                     0.0078125,
@@ -48,7 +46,7 @@
                                                     0.03125,
                                                     0.0625,
                                                     0.875976563*/
-                                                  };
+                                                        };
         /**
          * "Etaoin Shrdlu"
          * Taken from http://rinkworks.com/words/letterfreq.shtml
@@ -118,36 +116,28 @@
                                                       };
         */
         #endregion
-        public AritmeticCoder(string ?args)
+        public AritmeticCoder(string args)
         {
-            text = args ?? "abba";
-            code = 0.0m;
-            C = 0.0m;
-            A = 1.0m;
+            (text, code, C, A) = (args, 0.0m, 0.0m, 1.0m);
 
-            for (int index = 0; index < size; ++index)
+            for (var index = 0; index < size; ++index)
             {
                 letterFreqMap[letterFreqUnmap[index]] = index;
             }
 
-            fx = new List<decimal>(size);
-            Fx = new List<decimal>(size);
-
+            (fx, Fx) = (new List<decimal>(size), new List<decimal>(size));
             foreach (double _freq in letterFreq)
             {
                 // conversion (trimming) to finite binary representation
-                int bits = 12;
-                decimal freq = Convert.ToDecimal(_freq);
-                decimal precision = (1 << bits);
+                var bits = 13;
+                (decimal freq, decimal precision) = (Convert.ToDecimal(_freq), (1 << bits));
 
-                fx.Add(Convert.ToDecimal(Math.Floor(freq * precision) / precision));
+                fx.Add(Math.Floor(freq * precision) / precision);
             }
-            // computations of cummulative frequencies
+            // Cumulative frequencies
             Fx.Add(0.0m);
-            for (int i = 1; i < fx.Capacity; ++i)
-            {
-                Fx.Add(Fx[i - 1] + fx[i - 1]);
-            }
+            var freqs = fx.ToArray()[0..^1];
+            foreach (var fq in freqs) Fx.Add(Fx[^1] + fq);
         }
         ~AritmeticCoder()
         {
@@ -155,19 +145,23 @@
         }
         public void Dispose()
         {
-            Console.Out.WriteLine("Aftermath!");
+            Console.Out.WriteLine("Che sarà sarà...");
             GC.SuppressFinalize(this);
         }
         public void Encode()
         {
             foreach (char letter in text)
             {
-                int index = letterFreqMap[letter - 'a'];
+                var index = letterFreqMap[letter - 'a'];
 
                 C += Fx[index] * A;
                 A *= fx[index];
-
-                Console.Out.WriteLine($"[{C}, {C + A})");
+                
+                var width = 96;
+                var (c, a) = (Convert.ToInt32(C * width), Convert.ToInt32(A * width));                
+                Console.Out.WriteLine(new string(' ', c) + new string('.', 1 + a) 
+                                                         + new string(' ', 1 + width - c - a)
+                                                         + $"[{C}, {C + A})");
             }
             if (A == 0m)
             {
@@ -176,7 +170,7 @@
             code = C + A / 2m;
             /// "Binarization"
             {
-                int mb = Convert.ToInt32(Math.Ceiling(-Math.Log(Convert.ToDouble(A), 2))) + 1;
+                var mb = Convert.ToInt32(Math.Ceiling(-Math.Log(Convert.ToDouble(A), 2))) + 1;
 
                 Decimal binaryRange = Convert.ToDecimal(1 << mb);
                 Decimal coded_message = Math.Floor(code * binaryRange) / binaryRange;
@@ -186,17 +180,16 @@
 
         public void Decode()
         {
-            int length = text.Length;
+            var lenght = text.Length;
             text = "";
-
-            for (int i = 0; i < length; ++i)
+            for (var _ = 0; _ < lenght; ++_)
             {
-                int index = Extract(code);
+                var index = Extract(code);
 
                 code -= Fx[index];
                 code /= fx[index];
 
-                int letter = letterFreqUnmap[index];
+                var letter = letterFreqUnmap[index];
                 text += Convert.ToChar(letter + 'a');
             }
         }
@@ -217,37 +210,30 @@
         }
         private int Extract(decimal codeValue)
         {
-            for (int i = Fx.Capacity - 1; i > 0; --i)
-            {
-                if (codeValue >= Fx[i])
-                {
-                    return i;
-                }
-            }
-            return 0;
+            return Array.FindLastIndex(Fx.ToArray(), x => x < codeValue);
         }
-        static void Main(string[] args)
+        static void Main()
         {
             try
             {
                 while (true)
                 {
-                    string ?text = Console.ReadLine();
-                    if (text == "quit")
+                    // 'Nullity' check first...
+                    if (Console.ReadLine() is string text && text != "quit")
                     {
-                        break;
-                    }
-                    using AritmeticCoder codec = new(text);
-                    codec.Encode();
-                    codec.EncodingReport();
+                        using AritmeticCoder codec = new(text);
+                        codec.Encode();
+                        codec.EncodingReport();
 
-                    codec.Decode();
-                    codec.DecodingReport();
+                        codec.Decode();
+                        codec.DecodingReport();
+                    }
+                    else break;
                 }
             }
             catch (Exception e)
             {
-                Console.Error.WriteLine($"\nExpected exception has occured because: {e.Message}\n");
+                Console.Error.WriteLine($"\nExpected exception has occured: {e.Message}\n");
             }
         }
     }
